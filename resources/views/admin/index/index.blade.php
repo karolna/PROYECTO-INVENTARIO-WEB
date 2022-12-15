c812163212004e3da43e8f35e749ee94ef7b9a13
Karolina Guaman <karolinaguamn22@gmail.com>
2022-12-14 21:59:39 -0500

    cambio assets

--- resources/views/admin/index/index.blade.php
+++ resources/views/admin/index/index.blade.php
@@ -81,10 +81,10 @@
             <br><br><br> </div>
         <section >
             <div class="slick-center" style=" align:center; margin:auto "  >
-                <img src="/image/banner1.png" ">
-                <img src="/image/banner2.png" ">
-                <img src="/image/banner3.png" ">
-                <img src="/image/banner4.png" ">
+                <img src="{{asset('image/banner1.png')}}"> 
+                <img src="{{asset('image/banner2.png')}}">
+                <img src="{{asset('image/banner3.png')}}">
+                <img src="{{asset('image/banner4.png')}}">
             </div>
         </section>
         <!-- partial -->

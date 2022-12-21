8633d4eac8cebfefe46f69f7526a38db9a7e6d59
Karolina Guaman <karolinaguamn22@gmail.com>
2022-12-20 22:16:16 -0500

    cambios en ruta de face 16

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -52,7 +52,7 @@
                     @yield('create')
                     <li class="nav-item nav-profile dropdown">
                         <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" id="profileDropdown">
-                          <img src="../../melody/images/faces/face16.jpg" alt="profile"/>
+                          <img src="../../sistema/melody/images/faces/face16.jpg" alt="profile"/>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
 

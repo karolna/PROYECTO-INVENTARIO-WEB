498182ddae1cb3c760bcfe57716146b85a436b3d
Karolina Guaman <karolinaguamn22@gmail.com>
2022-12-22 14:56:09 -0500

    revision 26

--- app/Http/Controllers/ReserveController.php
+++ app/Http/Controllers/ReserveController.php
@@ -27,11 +27,11 @@ class ReserveController extends Controller
         //$reserves = Reserve::get();
        // $reserves = Reserve::where('status','VALIDO')->get();
        //$reserves =Reserve::where('status','VALIDO')->get();
-       $reserves=Reserve::whereDate('reserve_date', '<=', Carbon::now()->toDateTimeString())
-       ->whereDate('reserve_date', '>=',Carbon::now()->subDay()->toDateTimeString())->get();
+      // $reserves=Reserve::whereDate('created_at', '<=', Carbon::now()->toDateTimeString())
+       //->whereDate('created_at', '>=',Carbon::now()->subDay()->toDateTimeString())->get();
      // dd(Carbon::now()->subDay()->toDateTimeString(), Carbon::now()->toDateTimeString());
      // dd($reserves);
-      // $reserves=Reserve::whereBetween('created_at', [Carbon::now()->subDay()->toDateTimeString(),Carbon::now()->toDateTimeString()])->get();
+       $reserves=Reserve::whereBetween('created_at', [Carbon::now()->subDay()->toDateTimeString(), Carbon::now()->toDateTimeString()])->get();
 
       // dd(Carbon::now());
 

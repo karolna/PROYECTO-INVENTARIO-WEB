71c47f38db02223b7a83def9862f377390183720
Karolina Guaman <karolinaguamn22@gmail.com>
2022-12-22 11:40:37 -0500

    8

--- app/Http/Controllers/ReserveController.php
+++ app/Http/Controllers/ReserveController.php
@@ -30,7 +30,7 @@ class ReserveController extends Controller
     //   $reserves=Reserve::whereDate('reserve_date', '>=', Carbon::today()->toDateString())
       // ->whereDate('reserve_date', '<=',Carbon::now()->subDay()->toDateString())->get();
 
-       $reserves=Reserve::whereBetween('reserve_date', [ Carbon::today()->toDateString(), Carbon::now()->subDay()->toDateString()])->get();
+       $reserves=Reserve::whereBetween('reserve_date', [Carbon::now()->subDay()->toDateString(), Carbon::today()->toDateString()])->get();
       //($reserves);
       // dd(Carbon::now());
 

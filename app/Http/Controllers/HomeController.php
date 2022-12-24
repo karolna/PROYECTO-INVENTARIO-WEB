ae4a152c65768d0557cbcb264a7c3f3b7ad08a9a
Karolina Guaman <karolinaguamn22@gmail.com>
2022-12-23 22:17:07 -0500

    change lucid char

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,10 +24,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-      //  $comprasmes=DB::select('SELECT month(c.purchase_date) as mes, sum(c.total) as totalmes from purchases c where c.status="VALIDO" group by month(c.purchase_date) order by month(c.purchase_date) desc limit 25');
-      //  $ventasmes=DB::select('SELECT month(v.sale_date) as mes, sum(v.total) as totalmes from sales v where v.status="VALIDO" group by month(v.sale_date) order by month(v.sale_date) desc limit 50');
-      $comprasmes=DB::select('SELECT monthname(c.purchase_date) as mes, sum(c.total) as totalmes from purchases c where c.status="VALIDO" group by monthname(c.purchase_date) order by month(c.purchase_date) desc limit 12');
-     $ventasmes=DB::select('SELECT monthname(v.sale_date) as mes, sum(v.total) as totalmes from sales v where v.status="VALIDO" group by monthname(v.sale_date) order by month(v.sale_date) desc limit 12');
+        $comprasmes=DB::select('SELECT month(c.purchase_date) as mes, sum(c.total) as totalmes from purchases c where c.status="VALIDO" group by month(c.purchase_date) order by month(c.purchase_date) desc limit 25');
+        $ventasmes=DB::select('SELECT month(v.sale_date) as mes, sum(v.total) as totalmes from sales v where v.status="VALIDO" group by month(v.sale_date) order by month(v.sale_date) desc limit 50');
+     // $comprasmes=DB::select('SELECT monthname(c.purchase_date) as mes, sum(c.total) as totalmes from purchases c where c.status="VALIDO" group by monthname(c.purchase_date) order by month(c.purchase_date) desc limit 12');
+     //$ventasmes=DB::select('SELECT monthname(v.sale_date) as mes, sum(v.total) as totalmes from sales v where v.status="VALIDO" group by monthname(v.sale_date) order by month(v.sale_date) desc limit 12');
 
         $ventasdia=DB::select('SELECT DATE_FORMAT(v.sale_date,"%d/%m/%Y") as dia, sum(v.total) as totaldia from sales v where v.status="VALIDO" group by v.sale_date order by day(v.sale_date) asc limit 20');
         $totales=DB::select('SELECT (select ifnull(sum(c.total),0) from purchases c where DATE(c.purchase_date)=curdate() and c.status="VALIDO") as totalcompra, (select ifnull(sum(v.total),0) from sales v where DATE(v.sale_date)=curdate() and v.status="VALIDO") as totalventa');

01690c9941966d149130d9a48b658ae007a7cd85
Karolina Guaman <karolinaguamn22@gmail.com>
2022-12-26 20:33:48 -0500

    Inicio de sesion languaje

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card" style="margin-top:20%">
-                <div class="card-header">Iniciar sesión</div>
+                <div class="card-header">Inicio de sesión</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -53,8 +53,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary" style="background-color: #1abc9c; border-color:#1abc9c" >
-                                    {{ __('Login') }}
+                                <button type="submit" class="btn btn-primary" style="background-color: #1abc9c; border-color:#1abc9c" > Iniciar Sesión
                                 </button>
                             </div>
                         </div>

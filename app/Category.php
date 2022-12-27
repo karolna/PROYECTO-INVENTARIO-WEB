cd716be17f2c363d657aec49985392ad4b2189f9
Karolina Guaman <karolinaguamn22@gmail.com>
2022-12-26 21:32:58 -0500

    category changes 1

--- app/Category.php
+++ app/Category.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $fillable = [
-        'name', 'description',
+        'name', 'description','deleted_at'
     ];
     public function products(){
         return $this->hasMany(Product::class);

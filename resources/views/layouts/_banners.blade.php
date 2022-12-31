12cba2cef2f38721dbef041a08d70a43d2d5fb45
Karolina Guaman <karolinaguamn22@gmail.com>
2022-12-30 19:56:27 -0500

    change banners

--- resources/views/layouts/_banners.blade.php
+++ resources/views/layouts/_banners.blade.php
@@ -8,3 +8,32 @@
         <img src="{{asset('image/banner4.png')}}">
     </div>
 </section>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item ">
+            <a class="nav-link" href="{{ route('reserve.reserve_all') }}">Reservas</a>
+          </li>
+        <li class="nav-item ">
+          <a class="nav-link" href="{{ route('index.reencauchadas') }}">Llantas reencauchadas</a>
+        </li>
+        <li class="nav-item ">
+            <a class="nav-link" href="{{ route('index.nuevas') }}">Llantas nuevas </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="{{ route('index.tubos') }}">Tubos </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="{{ route('index.defensas') }}">Defensas </a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link" href="{{ route('index.aros') }}">Aros </a>
+          </li>
+      </ul>
+    </div>
+  </nav>
